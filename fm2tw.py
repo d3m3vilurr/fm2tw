3e86bbac41a038ed13df5c2c3210090505f5d3c3
Sunguk Lee <d3m3vilurr@gmail.com>
2021-01-29 03:16:02 +0900

    Fix duplicate bomb
    check timeline before sending new posting

--- fm2tw.py
+++ fm2tw.py
@@ -16,6 +16,7 @@ DATE_FORMAT = "%a, %d %b %Y %H:%M:%S +0000"
 DEFAULT_POST_FORMAT = "#NowPlaying \"{title}\" via Last.fm {link}"
 CONFIG = yaml.load(open('config.yaml'))
 DB_SESSION = None
+TWEEPY_SESSION = None
 
 def open_storage():
     global DB_SESSION
@@ -28,6 +29,21 @@ def open_storage():
         cursor.execute("PRAGMA user_version = %d" % STORE_VERSION)
         DB_SESSION = conn
     return DB_SESSION
+
+def open_tweepy():
+    global TWEEPY_SESSION
+    if not TWEEPY_SESSION:
+        auth = tweepy.OAuthHandler(
+            CONFIG["CONSUMER_KEY"],
+            CONFIG["CONSUMER_SECRET"]
+        )
+        auth.set_access_token(
+            CONFIG["ACCESS_TOKEN_KEY"],
+            CONFIG["ACCESS_TOKEN_SECRET"]
+        )
+        TWEEPY_SESSION = tweepy.API(auth)
+    return TWEEPY_SESSION
+
 
 def upgrade_storage(db, old_version, new_version):
     curr_version = old_version
@@ -85,16 +101,39 @@ def _get_title(scrob):
     return ' - '.join((scrob.get('artist').get('#text'),
                        scrob.get('name')))
 
-def _check_exist(scrob, last):
+def _make_twitter_title(title):
+    return len(title) < 100 and title or (title[:100] + '...')
+
+def _make_message(scrob, duplicate=0, post_format=None):
+    title = _make_twitter_title(_get_title(scrob))
+    return post_format.format(
+        title=title, link=scrob.get('url'),
+        duplicate=duplicate and ("(%d)" % duplicate) or ""
+    )
+
+def _exists_in_recent_twitter(scrob):
+    title = _get_title(scrob)
+    api = open_tweepy()
+    timeline = api.user_timeline()
+    twit_title = _make_twitter_title(title)
+    if any((twit_title in x.text) for x in timeline):
+        print("FOUND SAME MUSIC: %s" % title)
+        return True
+
+def _is_old_music(scrob):
+    updated = datetime.datetime \
+                      .fromtimestamp(int(scrob.get('date').get('uts')))
+    if (updated <= \
+        datetime.datetime.utcnow() - datetime.timedelta(10./24/60)):
+        print("SKIP OLD MUSIC: %s" % title)
+        return True
+
+def _exists_in_db(scrob, last, post_format=None):
     title = _get_title(scrob)
     updated = datetime.datetime \
                       .fromtimestamp(int(scrob.get('date').get('uts')))
     updated_range = (updated - datetime.timedelta(1./24)) \
                         .strftime("%Y-%m-%d %H:%M:%S")
-    if (updated <= \
-        datetime.datetime.utcnow() - datetime.timedelta(10./24/60)):
-        print("SKIP OLD PLAY MUSIC: %s" % title)
-        return True
     if (last and last.get('message') == title and \
         updated_range <= last.get('updated')):
         print("SKIP SAME MUSIC: %s" % title)
@@ -117,17 +156,10 @@ def _save_storage(scrob):
 
 def _post_twitter(scrob, post_format=None):
     post_format = post_format or DEFAULT_POST_FORMAT
-    auth = tweepy.OAuthHandler(
-        CONFIG["CONSUMER_KEY"],
-        CONFIG["CONSUMER_SECRET"]
-    )
-    auth.set_access_token(
-        CONFIG["ACCESS_TOKEN_KEY"],
-        CONFIG["ACCESS_TOKEN_SECRET"]
-    )
-    api = tweepy.API(auth)
+    api = open_tweepy()
     dup = 0
-    while True:
+    while not _exists_in_recent_twitter(scrob):
+        print("NOT EXISTS IN TWITTER")
         title = _get_title(scrob)
         title = len(title) < 100 and title or (title[:100] + '...')
         msg = post_format.format(
@@ -138,20 +170,23 @@ def _post_twitter(scrob, post_format=None):
             api.update_status(status=msg)
             return
         except tweepy.TweepError:
-            dup += 1
-            if dup >= 500:
-                raise
+            #dup += 1
+            #if dup >= 100:
+            #    raise
+            return
 
 def new_post(scrob, last, post_format=None):
     title = _get_title(scrob)
-    print("NEW POST MUSIC: %s" % title)
-    _save_storage(scrob)
+    print("TRY POST MUSIC: %s" % title)
     _post_twitter(scrob, post_format)
+    if _exists_in_db(scrob, last, post_format):
+        return
+    _save_storage(scrob)
 
 def main():
     last = last_post()
     scrob = get_lastfm(CONFIG["LASTFM_KEY"], CONFIG["LASTFM_USER"])
-    if _check_exist(scrob, last):
+    if _is_old_music(scrob):
         return
     new_post(scrob, last, CONFIG.get("POST_FORMAT"))
 
